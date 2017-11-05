17d10cd6a61f1f8837d6315e8cfd2e691218867a
mitsuru793 <mitsuru793@gmail.com>
2017-11-05 16:00:53 +0900

    add ValueObject.php

---
+++ src/ValueObject.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace PhpSnippetPost;
+
+/**
+ * Class ValueObject
+ *
+ * Wrapper class for scalar value.
+ *
+ * new instance only 'of' method, but construct from outside.
+ *
+ * @package PhpSnippetPost
+ */
+abstract class ValueObject implements \JsonSerializable
+{
+    protected $value;
+
+    protected function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    public static function of($value)
+    {
+        return new static($value);
+    }
+
+    public function isEmpty(): bool
+    {
+        return empty($this->value);
+    }
+
+    public function isNotEmpty(): bool
+    {
+        return !empty($this->value);
+    }
+
+    public function jsonSerialize()
+    {
+        return $this->value;
+    }
+
+    public function value()
+    {
+        return $this->value;
+    }
+}
