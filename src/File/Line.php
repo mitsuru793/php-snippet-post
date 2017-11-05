0ed2bfbf5b8363eaa6d122f60d9dfdbed150cdf7
mitsuru793 <mitsuru793@gmail.com>
2017-11-05 16:01:03 +0900

    add Line.php and its test

---
+++ src/File/Line.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace PhpSnippetPost\File;
+
+use PhpSnippetPost\ValueObject;
+
+/**
+ * Class Line
+ *
+ * First, call setCommentStartPattern and setCommentEndPattern.
+ *
+ * @package PhpSnippetPost\File
+ */
+class Line extends ValueObject
+{
+    /** @var string */
+    static private $commentStartPattern;
+
+    /** @var string */
+    static private $commentEndPattern;
+
+    /** @var string */
+    protected $value;
+
+    public function __construct(string $value)
+    {
+        parent::__construct($value);
+    }
+
+    public static function setCommentStartPattern(array $patterns): void
+    {
+        self::$commentStartPattern = implode('|', $patterns);
+    }
+
+    public static function setCommentEndPattern(array $patterns): void
+    {
+        self::$commentEndPattern = implode('|', $patterns);
+    }
+
+    public function isCommentStart(): bool
+    {
+        return preg_match('~^(' . self::$commentStartPattern . ')$~', $this->value);
+    }
+
+    public function isCommentEnd(): bool
+    {
+        return preg_match('~^(' . self::$commentEndPattern . ')$~', $this->value);
+    }
+}
---
+++ tests/PhpSnippetPost/File/LineTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace PhpSnippetPost\File;
+
+use PHPUnit\Framework\TestCase;
+
+class LineTest extends TestCase
+{
+    public function testConstruct()
+    {
+        $line = Line::of('val');
+        $this->assertSame('val', $line->value());
+    }
+
+    /**
+     * @dataProvider additionIsCommentStart
+     */
+    public function testIsCommentStart(string $value, bool $expected)
+    {
+        Line::setCommentStartPattern(['/\*\*', '<!--']);
+        $line = Line::of($value);
+        $this->assertSame($expected, $line->isCommentStart());
+    }
+
+    public function additionIsCommentStart()
+    {
+        return [
+            ['/**', true],
+            ['<!--', true],
+            ['/** ', false],
+            [' /**', false],
+            ['//', false],
+        ];
+    }
+
+    /**
+     * @dataProvider additionIsCommentEnd
+     */
+    public function testIsCommentEnd(string $value, bool $expected)
+    {
+        Line::setCommentEndPattern(['\*\*/', '-->']);
+        $line = Line::of($value);
+        $this->assertSame($expected, $line->isCommentEnd());
+    }
+
+    public function additionIsCommentEnd()
+    {
+        return [
+            ['**/', true],
+            ['-->', true],
+            ['**/ ', false],
+            [' **/', false],
+            ['//', false],
+        ];
+    }
+}
