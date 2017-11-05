a165400c84539ab35c17e2595d8da956f6cc86d2
mitsuru793 <mitsuru793@gmail.com>
2017-11-05 17:09:15 +0900

    add Lines.php and its test

---
+++ src/File/Lines.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace PhpSnippetPost\File;
+
+use Illuminate\Support\Collection;
+
+class Lines extends Collection
+{
+    /**
+     * Lines constructor.
+     * @param array <Line> $lines
+     */
+    public function __construct(array $lines)
+    {
+        parent::__construct($lines);
+    }
+
+    /**
+     * construct from file
+     * @param string $filePath
+     * @return Lines
+     */
+    public static function fromFile(string $filePath): self
+    {
+        $lines = explode(PHP_EOL, file_get_contents($filePath));
+        $lines = array_map(function ($line) {
+            return new Line($line);
+        }, $lines);
+        return new static($lines);
+    }
+
+    /**
+     * Get comment block without start and end lines at top.
+     * @param string $startPattern of front matter
+     * @param string $endPattern of front matter
+     * @return Lines
+     */
+    public function frontMatter(array $startPattern, array $endPattern): self
+    {
+        Line::setCommentStartPattern($startPattern);
+        Line::setCommentEndPattern($endPattern);
+
+        /** @var $inComment
+         *  need when
+         *    1. line comment start and end pattern are same.
+         *    2. no comment block
+         */
+        $inComment = false;
+        $lines = [];
+        $this->each(function ($line) use (&$lines, &$inComment) {
+            /** @var Line $line */
+            if (!$inComment && $line->isCommentStart()) {
+                $inComment = true;
+                return true;
+            }
+            if ($line->isCommentEnd()) {
+                return false;
+            }
+
+            if ($inComment) {
+                $lines[] = $line;
+            }
+        });
+        return new static($lines);
+    }
+}
---
+++ tests/PhpSnippetPost/File/LinesTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace PhpSnippetPost\File;
+
+use org\bovigo\vfs\vfsStream;
+use PHPUnit\Framework\TestCase;
+
+class LinesTest extends TestCase
+{
+    public function testFromFile()
+    {
+        $root = vfsStream::setup();
+        $file = $root->url() . '/text';
+        $expected = [
+            'v1',
+            'v2',
+        ];
+        $content = implode(PHP_EOL, $expected);
+        file_put_contents($file, $content);
+
+        $lines = Lines::fromFile($file);
+        $this->assertInstanceOf(Lines::class, $lines);
+
+        $actual = ($lines->map(function ($line) {
+            return $line->value();
+        })->all());
+        $this->assertSame($expected, $actual);
+
+        // TODO missing file
+    }
+
+    public function testFrontMatter()
+    {
+        $content = [
+            '--',
+            'val1',
+            '',
+            'val2',
+            '--',
+            'content',
+        ];
+        $lines = array_map(function ($line) {
+            return Line::of($line);
+        }, $content);
+        $lines = new Lines($lines);
+
+        $frontMatter = $lines->frontMatter(['--'], ['--']);
+        $this->assertInstanceOf(Lines::class, $frontMatter);
+
+        $expected = [
+            'val1',
+            '',
+            'val2',
+        ];
+        $actual = ($frontMatter->map(function ($line) {
+            return $line->value();
+        })->all());
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testFrontMatterWithoutCommentBlock()
+    {
+        $content = [
+            'val',
+            'content',
+        ];
+        $lines = array_map(function ($line) {
+            return Line::of($line);
+        }, $content);
+        $lines = new Lines($lines);
+
+        $frontMatter = $lines->frontMatter(['--'], ['--']);
+        $this->assertTrue($frontMatter->isEmpty());
+    }
+}
